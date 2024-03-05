5488ae927f3dcb21ee43ef1c32992f5e441d3e69
Menarg <erik.hahn.nilsson@gmail.com>
2024-03-05 13:05:51 +0100

    donesoes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,29 @@ import Product from './components/Product/Product';
 
 function App() {
 
+  const Products = [
+    { title: 'Help' , id: 1 },
+    { title: 'Skibidiboop' , id: 2 },
+    { title: 'The Benefits of gaming' , id: 3 },
+    { title: 'How to beome a God' , id: 4 },
+    { title: 'Dragons, how to court them and immortality' , id: 5 }
+];
+
+const ProductComponents = Products.map((Products) => {
+  return <Product title = {Products.title} key = {Products.id} />
+});
+
   return (
     <>
       <Header />
       <section className='ProductPage'>
-        <Product book='Help' />
-        <Product />
-        <Product />
-        <Product />
+
+        { ProductComponents }
+        {/* <Product book='Help' />
+        <Product book='Skibidiboop' />
+        <Product book='The Benefits of gaming' />
+        <Product book='How to beome a God' />
+        <Product book='Dragons, how to court them and immortality' /> */}
       </section>
     </>
   )
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,12 @@
 import './Product.css';
 
-function Product (Props) {
+function Product (Products) {
 
-    // Props
+    const { title } = Products; 
 
     return (
         <section className='Product'>
-            <h2>{Props.book}</h2>
+            <h2>{ title }</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae tenetur, molestiae voluptatum saepe, qui odio sunt similique error ratione laudantium molestias quam quibusdam culpa. Voluptate velit alias id totam aperiam architecto odit voluptates, magni, vel voluptatibus tempora culpa exercitationem repellendus dolorem suscipit explicabo.
             </p>
         </section>
