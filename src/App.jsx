57de1ab1b9883f942d0c439fe06b2725e0449b64
Menarg <erik.hahn.nilsson@gmail.com>
2024-03-12 13:38:06 +0100

    yeeaaahhhh

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Product from './components/Product/Product';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-
   const [amount, setAmount] = useState(0);
 
-  const [Products, setProducts] = useState([
-    { title: 'Help' , id: '1' },
-    { title: 'Skibidiboop' , id: '2' },
-    { title: 'The Benefits of gaming' , id: '3' },
-    { title: 'How to beome a God' , id: '4' },
-    { title: 'Dragons, how to court them and immortality' , id: '5' },
-  ]);
+  const [Products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://dummyjson.com/products');
+      const data = await response.json();
+      console.log(data);
+      setProducts(data.products);
+    }
+  
+    if (Products.length === 0) {
+      fetchData();
+    } else {console.log(error)};
+  }, []); // körs bara vid sidladdning
 
   const ProductComponents = Products.map((product) => {
-    return <Product title = {product.title} key = {product.id} setAmount = {setAmount} amount = {amount} />
+    return <Product title = {product.title} description = {product.description} key = {product.id} setAmount = {setAmount} amount = {amount} />
   });
 
   return (
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,7 +3,7 @@ import './Product.css';
 function Product (props) {
 
     // console.log(Products.title);
-    const { title, setAmount, amount } = props; 
+    const { title, description, setAmount, amount } = props; 
 
     function handleClick() {
         setAmount(amount + 1);
@@ -12,7 +12,7 @@ function Product (props) {
     return (
         <section className='Product'>
             <h2>{ title }</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae tenetur, molestiae voluptatum saepe, qui odio sunt similique error ratione laudantium molestias quam quibusdam culpa. Voluptate velit alias id totam aperiam architecto odit voluptates, magni, vel voluptatibus tempora culpa exercitationem repellendus dolorem suscipit explicabo.</p>
+            <p>{ description }</p>
             <button className='CartButton' onClick={handleClick}>Add to Cart</button>
         </section>
     );
